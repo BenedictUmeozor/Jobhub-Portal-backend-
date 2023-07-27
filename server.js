0354d6b1794a87d4e7824dfe686fbbc797a54ae8
Benedict Umeozor <137875050+BenedictUmeozor@users.noreply.github.com>
2023-07-27 15:06:06 +0100

    Update server.js
    Added cors origin

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://jobhub-portal-frontend.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
